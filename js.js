e5c230f0451b98cc8fa6986687b2c8a55862f896
skyknigoo <75960433+skyknigoo@users.noreply.github.com>
2020-12-14 10:14:55 +0800

    小專的作品試放

---
+++ js.js
@@ -0,0 +1,261 @@
+// 角色介紹點擊更換圖片介紹
+function pro_sim(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/西蒙無字.png'><p class='pro_text'>西蒙該作的主角。<br>起初沉默寡言，由於挖掘技術超群所以受到村長的關照，但被村中的少女們所瞧不起。後在大哥卡米那的帶領下，成為了勇於突破一切的男子漢。</p>"
+
+    ;
+
+}
+function pro_kmina(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/卡米那無字.png'><p class='pro_text'>卡米那該作中的靈魂角色，是熱情充滿野心的團隊領導。<br>追求浪漫方面有些高尚的個性，人格魅力非常高。與弟弟西蒙的個性完全不同，不過2人倒是相當絕妙的組合。</p>"
+    ;
+
+}
+function pro_yoko(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/傭子無字.png'><p class='pro_text'>庸子該作女主角之一，基哈村隔壁的地上部落“利突納村（Ritona）”出身。紅蓮團第一的狙擊手。擁有與年齡不符的成熟身段，容姿秀麗的少女。</p>";
+
+}
+function pro_nia(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/妮雅無字.png'><p class='pro_text'>妮亞該作的第二女主角，螺旋王羅杰諾姆的女兒。</p>";
+
+}
+function pro_leeron(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/利瓏.png'><p class='pro_text'>利瓏，“大紅蓮團”的天才機械技師，行為舉止非常女性化。之後在“大紅蓮”上頭擔任通訊和技術人員。在新政府成立後，擔任科學局局長。處事冷靜客觀，機械水平極佳。相當於大紅蓮團頭腦一般的存在。貌似是個不會衰老的可怕人物。</p>";
+
+}
+function pro_dayakka(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/達雅卡.png'><p class='pro_text'>達雅卡，原為利突那村的村長，後以顏面駕駛員身份加入大紅蓮團的戰鬥，最後坐鎮於大紅蓮的艦橋擔任總指揮。之後與奇揚結婚並育有一女叫做“安妮”，是全世界的第一百萬個人口，也是反螺旋族發動侵略的導火線。</p>";
+
+}
+function pro_coco(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/可可爺爺.png'><p class='pro_text'>可可爺爺，神出鬼沒的老人，同時在“大紅蓮團”擔任廚師。在尼婭有難的時候會現身並伸出援手，是螺旋王創造給妮亞的管家獸人。</p>";
+
+}
+function pro_rossiu(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/羅修.png'><p class='pro_text'>羅修，該作中的主要角色，出身自地下村落“亞哈卡村”，為人刻板，信奉宗教，是“紅蓮”的第二位駕駛員，同西蒙參與了討伐螺旋王的戰爭。在七年後，擔任政府的第二高官。在對反螺旋族戰爭結束後，接任了政府首腦的位置，二十年後將人類的繁榮推向了一個巔峰。</p>";
+
+}
+function pro_kittan(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/奇坦.png'><p class='pro_text'>奇坦，獸人獵人“黑之兄弟”領導人長子。血氣方剛的勁頭毫不遜於神名，性格上與神名也很相像。</p>";
+
+}
+function pro_kiyoh(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/奇揚.png'><p class='pro_text'>奇揚，“黑之兄弟”中的長女。擁有不輸給庸子的火爆身材。和平時期與達雅卡結婚並育有一女。</p>";
+
+}
+function pro_kinon(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/奇儂.png'><p class='pro_text'>奇儂，“黑之兄弟”中的次女。是個平日中總是悠閒自得的眼鏡娘。之後在“大紅蓮”上頭擔任雷達管制人員。喜歡露修。七年後成為新政府一員，輔助羅修。</p>";
+
+}
+function pro_kiyal(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/奇雅露.png'><p class='pro_text'>奇雅露，黑之兄弟”中的三女。虎牙妹一枚，受到大哥的影響，言語有些粗暴。</p>";
+}
+function pro_pota(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/布塔.png'><p class='pro_text'>布塔，西蒙的鼴鼠豬，一直伴隨西蒙戰鬥，擁有螺旋力，神奇的是無論是七年後還是二十年後，西蒙、庸子等人變老了普泰還是沒有成長，和利瓏一樣是個神奇的存在。<br>大紅蓮團的緊急糧食。</p>";
+}
+function pro_vlalo(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/維拉爾.png'><p class='pro_text'>維拉爾，一位神秘的獸人，負責消滅地面上的人類。 </p>";
+
+}
+function pro_bos1(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/怒濤王.png'><p class='pro_text'>怒濤的齊米洛夫夫，獸人四天王之一。</p>";
+
+}
+function pro_bos2(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/淫蕩王.png'><p class='pro_text'>流麗的阿蒂尼，獸人四天王之一。</p>";
+
+}
+function pro_bos3(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/神速王.png'><p class='pro_text'>神速的修托曼德拉，獸人四天王中唯一擅長空戰的存在。</p>";
+
+}
+function pro_bos4(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/不動王.png'><p class='pro_text'>不動的古阿姆，獸人四天王中最老的最強的，從螺旋王少年時陪伴在其身邊，漫長的壽命使他對於螺旋王的性格十分了解，是除了比拉魯唯一敢向螺旋王進諫的獸人。</p>";
+
+}
+function prb_BIGboss(){
+    x = document.getElementById("proimgs");
+    x.innerHTML="<img src='./imgs/角色介紹/15話之前/人物圖/螺旋王.png'><p class='pro_text'>螺旋王羅傑諾姆，獸人一族的創造者，螺旋族真正的守護者，曾經向反螺旋族發出挑戰，最終失敗，無奈之下只好通過封印人類於地下，來保護螺旋族。</p>";
+}
+    
+//角色表格切換 目前沒用
+function table1to_01(){
+    x = document.getElementById("table_01")
+}
+
+// 機體介紹點擊更換圖片
+function mec_lagann(){
+    x = document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/螺巖.png'><p class='pro_text'>Lagann<br>西蒙在挖掘工作中挖出的神秘機器人。 似乎是對核心鑽投的回應……？西蒙的精神起伏影響他的能力。通過與所在的Gurren合併，它成為“ Gurren Lagann”。</p>"
+}
+function mec_gurren() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/紅蓮.png'><p class='pro_text'>Gurren<br>卡米納搶過來的機體。重點是巨大誇張的墨鏡，也可以是迴旋鏢。結合西蒙的駕駛的Lagann。它變成了“ Gurren Lagann”。</p>"
+ }
+function mec_gurrenlagann() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/紅蓮螺巖.png'><p class='pro_text'> Gurren Lagann-紅蓮螺巖<br>西蒙的Lagann和卡米娜的Gurren的合體機器人。與眾多敵人作戰。從一開始就成為主要的機器人。</p>"
+ }
+function mec_gurrenlagannfinal() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/紅蓮螺巖飛行.png'><p class='pro_text'>Gurren Lagann-紅蓮螺巖(無特別名稱)<br>西蒙的Lagann和卡米娜的Gurren的合體機器人。在打倒四天王的時候搶來的飛行器與之合體擁有了飛行的能力。</p>"
+ }
+function mec_kittan() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/奇坦.png'><p class='pro_text'>帝王奇坦<br>“黑之兄弟”的基坦搶了獸人的機器人並佔為所有。 駕駛員奇坦和敏銳的造型使其更具攻擊性。</p>"
+ }
+function mec_yoko() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/庸子.png'><p class='pro_text'>達雅卡所操作的中程輔助類型，後來改造並給庸子使用。</p>"
+ }
+function mec_dagrla() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/大紅蓮團戰艦.png'><p class='pro_text'>大紅蓮戰艦<br>西蒙使用Lagann衝擊從怒濤王的手中所搶過來的戰艦，並成為大紅蓮團的戰艦也是家。</p>"
+ }
+function mec_dagrlaeve() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/大紅蓮團們.png'><p class='pro_text'>大紅蓮團團隊成員們。 每個機體都具有不同的型態及特色。</p>"
+ }
+function mec_arkglship() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/方舟紅蓮戰艦.png'><p class='pro_text'>方舟紅蓮<br>在卡米納市地底發現的超巨大戰艦。 它足夠容納300,000個城市和附近的居民。 這是螺旋王的遺產，羅修修理並試圖以此遠離地球，保護民眾。</p>"
+ }
+function mec_arkgl() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/方舟紅蓮機體.png'><p class='pro_text'>方舟紅蓮螺巖<br>由紅蓮螺巖和方舟紅蓮戰艦合體而成的超級機器人。 並能擊出穿越次元的一拳！</p>"
+ }
+function mec_galaxyglship() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/超銀河紅蓮戰艦.png'><p class='pro_text'> 聖殿螺巖艦<br>一種超超無畏巨大的機器人被反螺旋槳偽裝成月亮。 最初是螺旋王的最後武器“ 聖殿螺巖艦”。 通過螺旋鑽投重新獲得控制權，並成為大紅蓮團的新旗艦。<br>比月亮還要大</p>"
+ }
+function mec_galaxygl() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/超銀河紅蓮機體.png'><p class='pro_text'>超銀河紅蓮螺巖<br>通過改造進化超級銀河系紅蓮螺巖而誕生的<br>超絕螺旋<br>超弩級螺巖。</p>"
+ }
+function mec_vlalo() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/enki.png'><p class='pro_text'>鹽基<br>獸人維拉爾所駕駛的顏面，使用的武器是雙刀。</p>"
+ }
+function mec_bos1() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/怒濤王.png'><p class='pro_text'>白虎<br>怒濤的齊米洛夫夫所使用的顏面。 用長矛作為武器的攻擊具有致命的打擊。</p>"
+ }
+function mec_bos2() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/淫蕩王.png'><p class='pro_text'>青龍<br>流麗的阿蒂尼所駕駛的顏面。 它具有像蝴蝶一樣的巨大翅膀，並且可以自由操縱兩個鞭子。 可變形為蝎子型態。</p>"
+ }
+function mec_bos3() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/神速王.png'><p class='pro_text'>朱雀<br>神速的修托曼德拉所駕駛的顏面，速度是他的武器</p>"
+ }
+function mec_bos4() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/不動王.png'><p class='pro_text'>玄武<br>不動的古阿姆所駕駛的顏面，尖銳的針可在手臂自由伸縮，並可牢牢抱住敵人變形後，變成一個巨大的臉可用尖牙狠狠咬住敵人。</p>"
+ }
+function mec_bigboss_() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/螺旋王.png'><p class='pro_text'>羅善巖<br>螺旋王羅傑諾姆的專用顏面。 它具有與紅蓮螺巖相似的許多部分。</p>"
+ }
+function mec_bosshone() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/螺旋王基地.png'><p class='pro_text'>鐵佩林<br>獸人王國的真實樣子，是穿透雲層的龐大顏面。 螺旋國王羅傑諾姆的寶座位於其頂端。</p>"
+ }
+function mec_anti01() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/反螺旋族01.png'><p class='pro_text'>當地面上的人數達到100萬人時，反螺旋的先鋒。 它的結構不穩定，銷毀後會分解成小塊並爆炸。</p>"
+ }
+function mec_anti02() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/反螺旋族02.png'><p class='pro_text'>反螺旋的進攻部隊，而且進攻力，防禦力和破壞力都是重量級。 </p>"
+ }
+function mec_anti03() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/反螺旋族03.png'><p class='pro_text'>在宇宙中的反螺旋主力。 </p>"
+ }
+function mec_anti04() { 
+    x =document.getElementById("mecimgs");
+    x.innerHTML="<img src='./imgs/機體介紹/機體全圖/反/反螺旋族04.png'><p class='pro_text'>反螺旋領導的最終兵器。 憑藉其無與倫比的數量和超強的變形能力。</p>"
+ }
+
+// 經典台詞點擊顯示
+ function add_01() {
+    var x = document.getElementById("Mac1");
+    if (x.className.indexOf("add_new") == -1) {
+      x.className += "add_new";
+    } else {
+      x.className = x.className.replace("add_new", "");
+    }
+  }
+
+  function add_02() {
+    var x = document.getElementById("Mac2");
+    if (x.className.indexOf("add_new") == -1) {
+      x.className += "add_new";
+    } else {
+      x.className = x.className.replace("add_new", "");
+    }
+  }
+  function add_03() {
+    var x = document.getElementById("Mac3");
+    if (x.className.indexOf("add_new") == -1) {
+      x.className += "add_new";
+    } else {
+      x.className = x.className.replace("add_new", "");
+    }
+  }
+  function add_04() {
+    var x = document.getElementById("Mac4");
+    if (x.className.indexOf("add_new") == -1) {
+      x.className += "add_new";
+    } else {
+      x.className = x.className.replace("add_new", "");
+    }
+  }
+  function add_05() {
+    var x = document.getElementById("Mac5");
+    if (x.className.indexOf("add_new") == -1) {
+      x.className += "add_new";
+    } else {
+      x.className = x.className.replace("add_new", "");
+    }
+  }
+  function add_06() {
+    var x = document.getElementById("Mac6");
+    if (x.className.indexOf("add_new") == -1) {
+      x.className += "add_new";
+    } else {
+      x.className = x.className.replace("add_new", "");
+    }
+  }
+  function add_07() {
+    var x = document.getElementById("Mac7");
+    if (x.className.indexOf("add_new") == -1) {
+      x.className += "add_new";
+    } else {
+      x.className = x.className.replace("add_new", "");
+    }
+  }
+
+
+
+
+
+// 遊戲測試
+
